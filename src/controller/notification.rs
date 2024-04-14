3b1f5a04823692119e201deadc3e1f122e66f2c4
steven-fo <stevenfo0711@gmail.com>
2024-04-15 01:13:01 +0700

    fix: remove unused imports

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,7 +1,7 @@
 use rocket::serde::json::Json;
 
 use bambangshop_receiver::Result;
-use crate::model::notification::{self, Notification};
+use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::service::notification::NotificationService;
 
--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,13 +1,11 @@
 use std::thread;
 
-use rocket::http::hyper::request;
 use rocket::http::Status;
 use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
 
 use bambangshop_receiver::{APP_CONFIG, REQWEST_CLIENT, Result, compose_error_response};
-use crate::controller::notification;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::repository::notification::NotificationRepository;
